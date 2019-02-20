3a48477ec1e3dd2718d429eec6b17caf8e2644a0
wbarret1 <barrett.williamm@epa.gov>
2019-02-20 11:55:27 -0500

    Add project files.

---
+++ SustainableChemistryWeb/Controllers/FunctionalGroupsController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SustainableChemistryWeb.Models;
+
+namespace SustainableChemistryWeb.Controllers
+{
+    public class FunctionalGroupsController : Controller
+    {
+        private readonly SustainableChemistryContext _context;
+
+        public FunctionalGroupsController(SustainableChemistryContext context)
+        {
+            _context = context;
+        }
+
+        // GET: FunctionalGroups
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.AppFunctionalgroup.ToListAsync());
+        }
+
+        // GET: FunctionalGroups/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var appFunctionalgroup = await _context.AppFunctionalgroup
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (appFunctionalgroup == null)
+            {
+                return NotFound();
+            }
+
+            return View(appFunctionalgroup);
+        }
+
+        // GET: FunctionalGroups/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: FunctionalGroups/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Smarts,Image")] AppFunctionalgroup appFunctionalgroup)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(appFunctionalgroup);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(appFunctionalgroup);
+        }
+
+        // GET: FunctionalGroups/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var appFunctionalgroup = await _context.AppFunctionalgroup.FindAsync(id);
+            if (appFunctionalgroup == null)
+            {
+                return NotFound();
+            }
+            return View(appFunctionalgroup);
+        }
+
+        // POST: FunctionalGroups/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("Id,Name,Smarts,Image")] AppFunctionalgroup appFunctionalgroup)
+        {
+            if (id != appFunctionalgroup.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(appFunctionalgroup);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AppFunctionalgroupExists(appFunctionalgroup.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(appFunctionalgroup);
+        }
+
+        // GET: FunctionalGroups/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var appFunctionalgroup = await _context.AppFunctionalgroup
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (appFunctionalgroup == null)
+            {
+                return NotFound();
+            }
+
+            return View(appFunctionalgroup);
+        }
+
+        // POST: FunctionalGroups/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            var appFunctionalgroup = await _context.AppFunctionalgroup.FindAsync(id);
+            _context.AppFunctionalgroup.Remove(appFunctionalgroup);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AppFunctionalgroupExists(long id)
+        {
+            return _context.AppFunctionalgroup.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ SustainableChemistryWeb/Controllers/NamedreactionsController.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SustainableChemistryWeb.Models;
+
+namespace SustainableChemistryWeb.Controllers
+{
+    public class NamedreactionsController : Controller
+    {
+        private readonly SustainableChemistryContext _context;
+
+        public NamedreactionsController(SustainableChemistryContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Namedreactions
+        public async Task<IActionResult> Index()
+        {
+            var sustainableChemistryContext = _context.AppNamedreaction.Include(a => a.Catalyst).Include(a => a.FunctionalGroup).Include(a => a.Solvent);
+            return View(await sustainableChemistryContext.ToListAsync());
+        }
+
+        // GET: Namedreactions/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var appNamedreaction = await _context.AppNamedreaction
+                .Include(a => a.Catalyst)
+                .Include(a => a.FunctionalGroup)
+                .Include(a => a.Solvent)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (appNamedreaction == null)
+            {
+                return NotFound();
+            }
+
+            return View(appNamedreaction);
+        }
+
+        // GET: Namedreactions/Create
+        public IActionResult Create()
+        {
+            ViewData["CatalystId"] = new SelectList(_context.AppCatalyst, "Id", "Description");
+            ViewData["FunctionalGroupId"] = new SelectList(_context.AppFunctionalgroup, "Id", "Image");
+            ViewData["SolventId"] = new SelectList(_context.AppSolvent, "Id", "Description");
+            return View();
+        }
+
+        // POST: Namedreactions/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,ReactantA,ReactantB,ReactantC,Product,Heat,AcidBase,Image,CatalystId,FunctionalGroupId,SolventId,Url")] AppNamedreaction appNamedreaction)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(appNamedreaction);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CatalystId"] = new SelectList(_context.AppCatalyst, "Id", "Description", appNamedreaction.CatalystId);
+            ViewData["FunctionalGroupId"] = new SelectList(_context.AppFunctionalgroup, "Id", "Image", appNamedreaction.FunctionalGroupId);
+            ViewData["SolventId"] = new SelectList(_context.AppSolvent, "Id", "Description", appNamedreaction.SolventId);
+            return View(appNamedreaction);
+        }
+
+        // GET: Namedreactions/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var appNamedreaction = await _context.AppNamedreaction.FindAsync(id);
+            if (appNamedreaction == null)
+            {
+                return NotFound();
+            }
+            ViewData["CatalystId"] = new SelectList(_context.AppCatalyst, "Id", "Description", appNamedreaction.CatalystId);
+            ViewData["FunctionalGroupId"] = new SelectList(_context.AppFunctionalgroup, "Id", "Image", appNamedreaction.FunctionalGroupId);
+            ViewData["SolventId"] = new SelectList(_context.AppSolvent, "Id", "Description", appNamedreaction.SolventId);
+            return View(appNamedreaction);
+        }
+
+        // POST: Namedreactions/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("Id,Name,ReactantA,ReactantB,ReactantC,Product,Heat,AcidBase,Image,CatalystId,FunctionalGroupId,SolventId,Url")] AppNamedreaction appNamedreaction)
+        {
+            if (id != appNamedreaction.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(appNamedreaction);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AppNamedreactionExists(appNamedreaction.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CatalystId"] = new SelectList(_context.AppCatalyst, "Id", "Description", appNamedreaction.CatalystId);
+            ViewData["FunctionalGroupId"] = new SelectList(_context.AppFunctionalgroup, "Id", "Image", appNamedreaction.FunctionalGroupId);
+            ViewData["SolventId"] = new SelectList(_context.AppSolvent, "Id", "Description", appNamedreaction.SolventId);
+            return View(appNamedreaction);
+        }
+
+        // GET: Namedreactions/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var appNamedreaction = await _context.AppNamedreaction
+                .Include(a => a.Catalyst)
+                .Include(a => a.FunctionalGroup)
+                .Include(a => a.Solvent)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (appNamedreaction == null)
+            {
+                return NotFound();
+            }
+
+            return View(appNamedreaction);
+        }
+
+        // POST: Namedreactions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            var appNamedreaction = await _context.AppNamedreaction.FindAsync(id);
+            _context.AppNamedreaction.Remove(appNamedreaction);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AppNamedreactionExists(long id)
+        {
+            return _context.AppNamedreaction.Any(e => e.Id == id);
+        }
+    }
+}
